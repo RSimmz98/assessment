dfceb7d2b91312fe0404361913595202f4943a63
RSimmz98 <richsonsimbabure@gmail.com>
2022-09-29 00:01:53 +0200

    fixed the responsive

--- src/components/eventspage.js
+++ src/components/eventspage.js
@@ -2,15 +2,7 @@ import React from 'react'
 
 const Eventspage = () => {
   return (
-<<<<<<< HEAD
-    <div className='grid grid-cols-1 md:grid-cols-2-reverse '>
-      <p>This is card</p>
-      <div>this is a second card </div>
-      </div>
-
-=======
     <div>Eventpage</div>
->>>>>>> 82f05a7f25068e188dbd4088a1e554adc024aaa5
   )
 }
 
