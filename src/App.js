fd0ecb726b8e0d6b1abbef418ec6c29db0aa23dc
RSimmz98 <richsonsimbabure@gmail.com>
2022-09-29 00:03:15 +0200

    fixed the responsiveness

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from "./components/home"
-<<<<<<< HEAD
+
 import Eventspage from './components/eventspage';
-=======
->>>>>>> 82f05a7f25068e188dbd4088a1e554adc024aaa5
+
 import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
